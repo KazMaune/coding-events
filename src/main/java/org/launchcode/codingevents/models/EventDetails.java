294d28c5a96f4e1a01eeaeb6586fd836ac9129db
Kaz <kikainvr@gmail.com>
2022-11-07 14:50:20 -0600

    Finished

--- src/main/java/org/launchcode/codingevents/models/EventDetails.java
+++ src/main/java/org/launchcode/codingevents/models/EventDetails.java
@@ -12,11 +12,11 @@ public class EventDetails extends AbstractEntity {
     @Size(max = 500, message = "Description too long!")
     private String description;
 
-    @NotBlank
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email, Try again.")
     private String contactEmail;
 
-    public EventDetails(String description, String contactEmail) {
+    public EventDetails(@Size(max = 500, message = "Description is too long!") String description, @NotBlank(message = "Email is required") @Email(message = "Invalid email, Try again.") String contactEmail) {
         this.description = description;
         this.contactEmail = contactEmail;
     }
